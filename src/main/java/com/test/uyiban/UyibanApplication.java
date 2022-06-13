4ed57750f62239db8e04e0c1cc733ab6c3b069fe
Albert <albert@fzhxkj.com>
2022-06-13 10:34:56 +0800

    init

---
+++ src/main/java/com/test/uyiban/UyibanApplication.java
@@ -0,0 +1,13 @@
+package com.test.uyiban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UyibanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UyibanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/uyiban/UyibanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.uyiban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UyibanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
