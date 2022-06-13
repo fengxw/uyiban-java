6f8f4178aae7b9157e8efa088bd29d32b67c140b
onceme <onceatease@gmail.com>
2022-06-13 14:10:28 +0800

    Add:web api 接口demo

--- src/main/java/com/test/uyiban/UyibanApplication.java
+++ src/main/java/com/test/uyiban/UyibanApplication.java
@@ -2,12 +2,19 @@ package com.test.uyiban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //暂时不配置数据源
+@RestController
 public class UyibanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UyibanApplication.class, args);
 	}
-
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello Spring!";
+	}
 }
