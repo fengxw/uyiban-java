139c013818043fcdc2ee3eefe00c7919865efe83
Albert <albert@fzhxkj.com>
2022-06-13 16:54:32 +0800

    add notice application

---
+++ src/main/java/com/test/freshman/controller/admin/NoticeApplication.java
@@ -0,0 +1,46 @@
+package com.test.freshman.controller.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@RestController
+public class NoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeApplication.class, args);
+    }
+
+    @RequestMapping("/freshman/admin/notice/detail")
+    public String detail()
+    {
+        System.out.println("first java api");
+//        $batch = BatchModel::getInstance()->findOneBy([
+//            'UniversityId' => $this->universityId,
+//            'Id' => $batchId,
+//        ]);
+//        if (empty($batch)) {
+//            throw new OutputException('批次不存在');
+//        }
+//        $batch['EducationLevel'] = json_decode($batch['EducationLevel'], true);
+//
+//        $notice = NoticeModel::getInstance()->findBy([
+//            'UniversityId' => $this->universityId,
+//            'BatchId' => $batchId,
+//        ], 'EducationLevel, Title, Content, Attachments');
+//
+//        foreach ($notice as &$item) {
+//        if (!empty($item['Attachments'])) {
+//            $item['Attachments'] = json_decode($item['Attachments'], true);
+//        }
+//    }
+
+//        $batch['Notice'] = $notice;
+
+        return "batchId";
+//        return $batch;
+    }
+}
